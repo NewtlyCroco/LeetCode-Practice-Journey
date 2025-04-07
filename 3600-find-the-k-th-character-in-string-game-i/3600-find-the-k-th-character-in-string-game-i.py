9bfbb6a4d5ff86d7a5cbbb5e8ef4e9143b88b7ec
Will Mitchell <161643944+NewtlyCroco@users.noreply.github.com>
2025-04-07 07:04:18 -0600

    Time: 15 ms (31.23%), Space: 17.8 MB (38.77%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.py
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.py
@@ -2,16 +2,14 @@ class Solution:
     def kthCharacter(self, k: int) -> str:
         word = "a"
         letter_conversion = {"a" : "b", "b" : "c", "c" : "d", "d" : "e", "e" : "f", "f" : "g", "g" : "h", "h": "i", "i":"j", "j" : "k", "k" : "l", "l": "m", "m" : "n", "n" : "o", "o" : "p", "p" : "q", "q" : "r", "r" : "s", "s" : "t", "t" : "u", "u" : "v", "v" : "w", "w" : "x", "x" : "y", "y" : "z", "z" : "a"}
-        temp_letter_track = 1
+        temp_letter_track = 1 #hashmap for the win, although in a new instane of this problem, I can just iterate over characters like using a+1 as in bits this will be equivilent to b
 
         while(temp_letter_track <= k):
-            print(word)
-            print(temp_letter_track)
             temp_append = ""
-            temp_letter_track = len(word)
-            for char in word:
+            temp_letter_track = len(word)#was having a problem here, but fixed it, as we need to change length before processing, so we execute one more additional time, just based on how I have formated this code!
+            for char in word:#this is fulfulling the "At least k integers" part of the problem!
                 temp_append += letter_conversion[char]
             word += temp_append
            
 
-        return word[k-1]
+        return word[k-1]#this is because we are on zero index, so we need to look at the number before our final to get true value!
